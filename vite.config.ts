55c45720a5ba59a8f0c36701d14b7c704f54c8a1
praveen kumar saraswat <109068418+praveensaras@users.noreply.github.com>
2025-08-24 18:01:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/gkphoto-studio/',
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// })
+// @ts-ignore
 export default defineConfig({
+  base: '/gkphoto-studio/',  // Ye line add karo
   plugins: [react()],
-  base: '/gkphoto-studio/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
